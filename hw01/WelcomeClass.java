237fbd7a9a72e653354df3c1feee9959fb05c142
dankang1997 <kyk219@lehigh.edu>
2015-08-31 21:12:28 +0000

    updated hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,26 @@
+
+// This is a hw assignment due on Tuesday, Sept 1st.
+// We are to create a welcome class with our lehigh id our a signature
+
+
+public class WelcomeClass{
+
+    public static void main(String[] args){
+    
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-K--Y--K--2--1--9-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        
+        System.out.println("I moved to the U.S from South Korea when I was in 5th grade. It has been 8 years.");
+        
+    
+
+    
+    }
+    
+}
